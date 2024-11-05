e73a94cfbbbba240e8b47db3d9840722c1ceb200
Henrique Manoel Vieira <76623526+ziiklinux@users.noreply.github.com>
2024-11-05 15:50:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -12,3 +12,12 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+ 
+ The above code creates a simple HTTP server that listens on port 3000 and responds with "Hello, World!" when you visit it in your browser. 
+ To run the server, save the file and run the following command in the terminal: 
+ node comments.js
+ 
+ You should see the message  Server running at http://
+ 
+ This is a simple server that listens on port 3000 and responds with a 200 status code and a “Hello, World!” message. 
+ Let’s add the following code to the  app.js  file:
